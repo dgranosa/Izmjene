073d2581d9b304e0be9dd1a3b61e705d56983822
dgranosa <dgranosa@gmail.com>
2018-11-07 16:46:07 +0100

    Switched to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_06_210527) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "changes", force: :cascade do |t|
     t.date "date", null: false
     t.string "shift", null: false
