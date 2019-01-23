fae7e4205d17fc5e7ab34cf837fdf2f24c52fe9c
dgranosa <dgranosa@gmail.com>
2019-01-23 22:23:50 +0100

    No longer crashes when accessing weekend

--- app/controllers/changes_controller.rb
+++ app/controllers/changes_controller.rb
@@ -15,7 +15,7 @@ class ChangesController < ApplicationController
                 header: @header.to_a,
                 data: @data,
                 data2: @data2,
-                schedule: $schedule[params[:class]][params[:shift] == 'A' ? 1 : 0][@change.date.wday - 1][@header.first == -1 ? 6..14 : 1..9],
+                schedule: @change.date.wday.between?(1, 5) ? $schedule[params[:class]][params[:shift] == 'A' ? 1 : 0][@change.date.wday - 1][@header.first == -1 ? 6..14 : 1..9] : nil,
                 starttime: @starttime,
                 endtime: @endtime,
                 updated_at: @change.updated_at.to_s
